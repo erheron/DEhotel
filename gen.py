a4659967cb095da58bec47cb69e51ffd03af38f6
Vladyslav Rachek <v.rachek@student.uj.edu.pl>
2018-06-04 00:13:25 +0200

    1) Changes in create.sql - edited column's names 2) Little improvements in gen.py - now we can insert rows with Faker

--- gen.py
+++ gen.py
@@ -1,6 +1,14 @@
 import psycopg2
 from pprint import pprint
 from faker import Faker
+from random import randint
+
+#to generate random phone number
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
 
 
 #dbname = raw_input("Enter current database name: ")
@@ -9,29 +17,32 @@ from faker import Faker
 
 #TODO: problem with standart input and connecting
 try:
-	conn = psycopg2.connect("dbname = 'erheron' user = 'erheron' password ='erheron'")
+	conn = psycopg2.connect("dbname = 'hotel' user = 'erheron' password ='erheron'")
 except:
 	print('Unable to connect')
 
 cur = conn.cursor()
 
-fake = Faker()
-
-fake.name()
-
-fake.text()
+f_pl = Faker('pl_PL')
+f_us = Faker()
 
 class maker:
 	sql_insert="""INSERT INTO {}({columns}) VALUES({val});"""
 	
-	def insert_names(self, table_name, col, k):
+	def insert_names(self, table_name, col, k, faker):
 		for i in range(k):
-			random_name, random_surname = fake.name().split();
-			cur.execute(self.sql_insert.format(table_name, columns = col, val=str(i) + ",'" + random_name + "', '" + random_surname + "'"))
+			random_name, random_surname = faker.first_name(), faker.last_name();
+			#print random_name, random_surname
+			
+			#generating pseudo-random email
+			email=faker.email()
+			tel_number='0' + str(random_with_N_digits(8))
+			cur.execute(self.sql_insert.format(table_name, columns = col, val="'" + random_name + "', '" + random_surname + "', '" + tel_number + "', '"
+				+ email + "'"))
 
 		conn.commit()
 
 maker1 = maker()
-#rows = conn.fetchall()
-#for row in rows:
-#	print row
+
+maker1.insert_names('goscie', 'imie, nazwisko, nr_tel, email',10, f_pl) 
+maker1.insert_names('goscie', 'imie, nazwisko, nr_tel, email',100, f_us) 
