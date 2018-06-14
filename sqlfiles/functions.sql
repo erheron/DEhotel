3c5f4f7d32efe7dcc78e363d619d157cdec6e1e4
Katarzyna Król <katarzynaagata.krol@student.uj.edu.pl>
2018-06-14 01:09:00 -0700

    Funcion oblicz_znizke

---
+++ sqlfiles/functions.sql
@@ -0,0 +1,24 @@
+--oblicza znizke na podstawie liczby dni spedzonych w hotelu
+--[1,30) dni - 5% znizki
+--[30,...) dni 15% znizki
+create or replace function oblicz_znizke(e_mail varchar, cena numeric)
+	returns numeric as
+$$
+declare
+	suma_dni numeric;
+	id integer;
+begin
+	id = (select id_goscia from goscie g where g.email=e_mail);
+	suma_dni = coalesce((select sum(data_do - data_od)
+			from  rezerwacje_pokoje natural join rezerwacje_goscie
+			where id = id_goscia),0);
+	if suma_dni < 1 then
+		return round(cena,2);
+	elsif suma_dni >=1 and suma_dni < 30 then
+		return round(cena*0.95,2);
+	else
+		return round(cena*0.85,2); 
+	end if;
+end;
+$$
+language 'plpgsql';
